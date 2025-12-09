e0e92d07d1c92a8f6bf8e575211505865f724a27
Bell <riley.bell2355@gmail.com>
2025-12-09 12:07:22 +0000

    Commits

--- code/AdventureGame.java
+++ code/AdventureGame.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
 
 public class AdventureGame {
+
+    public static void main(String[] args){
+        // Grid Dimensions
+        int rows = 5, cols = 5, posR = 0, posC = 0;
+        int[][] grid = new int[rows][cols];
+
+        int pos = grid[0][0];
+
+        // Filling grid test
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                grid[r][c] = (r) * cols + c + 1;
+            }
+        }
+
+
+
+        // Print the grid
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                System.out.print(grid[r][c] + "\t");
+            }
+            System.out.println();
+        }
+
+       AdventureMove.Move(grid, pos, posR, posC, cols, rows);
+        
+    }
 }
--- code/AdventureMove.java
+++ code/AdventureMove.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class AdventureMove {
+public class AdventureMove  {
+    public static void Move(int[][] grid, int pos, int posR, int posC, int cols, int rows) {
+    
+
+    // Movement Input Test
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toUpperCase();
+
+        switch (input) {
+            case "UP":
+                if (posR > 0) posR--;
+                break;
+            case "DOWN":
+                if (posR < rows - 1) posR++;
+                break;
+            case "LEFT":
+                if (posC > 0) posC--;
+                break;
+            case "RIGHT":
+                if (posC < cols - 1) posC++;
+                break;
+            default:
+                System.out.println("Invalid command");
+        }
+
+        pos = grid[posR][posC];
+        System.out.println(pos);
+
+    }
 }
